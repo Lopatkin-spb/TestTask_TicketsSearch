4b5bd6a8284680db79188e29cd89df15e78ed88a
Lopatkin-spb <lopatkin.spb@mail.ru>
2024-07-13 15:53:55 +0400

    - migrated screen Profile;

--- presentation/src/main/java/com/example/testtask_ticketssearch/presentation/profile/ProfileFragment.kt
+++ presentation/src/main/java/com/example/testtask_ticketssearch/presentation/profile/ProfileFragment.kt
@@ -5,10 +5,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.testtask_ticketssearch.databinding.FragmentProfileBinding
 import com.example.testtask_ticketssearch.presentation.AppActivity
 import com.example.testtask_ticketssearch.presentation.ViewModelFactory
 import javax.inject.Inject
@@ -17,11 +32,6 @@ class ProfileFragment : Fragment() {
 
     @Inject
     lateinit var viewModelFactory: ViewModelFactory
-    private var _binding: FragmentProfileBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -35,18 +45,37 @@ class ProfileFragment : Fragment() {
     ): View {
         val viewModel = ViewModelProvider(this, viewModelFactory).get(ProfileViewModel::class.java)
 
-        _binding = FragmentProfileBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textStub
-        viewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                val text by viewModel.text.observeAsState()
+                Screen(text)
+            }
         }
-        return root
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+}
+
+@Composable
+private fun Screen(text: String?, modifier: Modifier = Modifier) {
+    MaterialTheme {
+        text?.let { text ->
+            Box(modifier.fillMaxSize()) {
+                Text(
+                    text = text,
+                    modifier = modifier.fillMaxWidth().wrapContentHeight().align(Alignment.Center),
+                    textAlign = TextAlign.Center,
+                    color = Color.White, //TODO: set from theme
+                    fontSize = 20.sp,
+                )
+            }
+        }
     }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+private fun ScreenPreview() {
+    val text = "ProfileFragment"
+    Screen(text)
 }
